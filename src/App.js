3446841fd12b682a88f39847a20f36f0708b9350
Christopher Boyle <cboyle824@gmail.com>
2021-07-16 06:15:21 -0400

    Implemented Character Component
    Names render on screen but none of the npm tests pass... not sure why?

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
+
+import React, { useEffect, useState } from "react";
+
+import Characters from "./components/Character";
+import axios from "axios";
 
 const App = () => {
+  const [data, setData] = useState("");
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(
+    () =>
+      axios
+        .get(`https://swapi.dev/api/people`)
+        .then((res) => setData(res.data))
+        .catch((err) => console.error(err)),
+    []
+  );
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {data.map((obj) => {
+        return <Characters name={obj.name} />;
+      })}
     </div>
   );
-}
+};
 
 export default App;
